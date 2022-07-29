26a99cc1f8c7692342f663a89d75c246f9519f43
wu-jldeyoung <97631463+wu-jldeyoung@users.noreply.github.com>
2022-07-29 11:48:17 -0700

    Added docs and src files
    Initial commit of documentation files and source code from research May-Jul '22.

---
+++ src/Utils/qscript.py
@@ -0,0 +1,67 @@
+# Author: jldeyoung on 2022-07-28 (this document as of 17:52)
+
+# Assumptions:
+# qemu-system-riscv64 is installed and configured correctly.
+# A working copy of the riscv-gnu-toolchain is built on the system,
+# with the environment variable $RISCV set to the toolchain build
+# directory. The build I used was the riscv-unknown-linux-gnu-,
+# configured with --enable-multilib.
+# The toolchain install directory has been added to PATH.
+
+import os
+import datetime as dt
+import subprocess as sp
+import shlex
+
+# For now, we'll go with ~/Desktop/riscv-hello-world/asm/hello
+# Config file? Leave hard-coded?
+
+# location of a linked ELF file compiled for riscv64
+# if Qemu gives "" error, check input file
+path = "/home/jldey/Desktop/riscv-hello-world/asm/hello"
+
+# commandline arguments for QEMU, tokenized according to shlex.split()
+args = ["qemu-system-riscv64", 
+		"-machine", "virt", 	# see QEMU documentation for more details, or use qemu-system-riscv64 -machine help
+		"-kernel", path, 		# launches the file at path on the guest system
+		"-monitor", "stdio", 	# sends QEMU monitor to stdio (specified below in the subprocess call)
+		"-S",					# starts the guest paused
+		"-d", "cpu",			# logs cpu state (register values) to file specified by -D
+		"-D", "./qpyLog"+dt.datetime.now().strftime("%Y%m%d-%H%M%S")+".txt"]
+								# creates log file with this name in the current working directory
+
+# copy current environment variables into a dict for use by subprocess
+env = os.environ.copy()
+
+# run QEMU with args as arguments
+# shell = True has the potential for shell injection, as noted in documentation
+# However, it may be that using shlex.join(), as done here, can mitigate this injection vulnerability.
+# env=env may be unnecessary here, but keeping it in seems like good practice for portability
+
+# stdin, stdout, and stderr can be directed to an existing file object.
+# subprocess.run() waits for a process to complete. Popen() may be more useful to us here.
+#s = sp.run(shlex.join(args), stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, text=True, env = env)
+
+# need a Popen object to communicate with child process
+# PIPE is a file object (stream?) that handles I/O to and from the child process.
+
+# The following results in undefined behavior when called on the list args
+# but executes as expected when called on args joined into a single string.
+s = sp.run(shlex.join(args), shell=True, text=True, env=env)
+
+
+# THREE EQUIVALENT EXPRESSIONS:
+#==============================
+#os.system(" ".join(args))
+#retcode = sp.call(" ".join(args), shell=True)
+#s = sp.run(" ".join(args), shell=True, text=True)
+#==============================
+
+# pipe "info registers\n" to QEMU
+
+
+# tell the monitor to continue execution ("c\n")
+
+# loop time! Do a couple that send "info registers\n"
+
+#quit QEMU
