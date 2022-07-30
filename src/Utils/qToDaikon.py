0394d39f201782ca4c96ab655fe9469745e79a15
juni <jldeyoung@willamette.edu>
2022-07-29 17:57:22 -0700

    Added qToDaikon.py with header information

---
+++ src/Utils/qToDaikon.py
@@ -0,0 +1,31 @@
+# Author: wu-jldeyoung on 2022-07-29
+#
+# Assumptions:
+# ====================
+# qemu-system-riscv64 is installed and configured correctly.
+#
+# A working copy of the riscv-gnu-toolchain is built on the
+# system, with the environment variable $RISCV set to the
+# toolchain build directory. The build I used was the
+# riscv-unknown-linux-gnu- variant, configured with --enable-multilib.
+#
+# The toolchain install directory has been added to PATH.
+# 
+# Input:
+# ====================
+# A .txt file with register values as produced by QEMU's 
+# built-in logging function or the monitor output from 
+# `info registers`, at the path specified below.
+# 
+# Output:
+# ====================
+# A properly formatted Daikon trace in the current working 
+# directory, with the same filename as the input .txt.
+#
+# ====================
+
+#import ???
+
+txtIn = "file_to_parse.txt"
+
+open(txtIn, "rt")
